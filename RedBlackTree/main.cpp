51bcbefd347bfb0113a4741eed647ed2280c8ce4
Aaron Cohn <acohn@cogswell.edu>
2013-04-24 01:44:27 -0700

    Implemented randomized testing of insertion and removal from red-black tree

--- RedBlackTree/main.cpp
+++ RedBlackTree/main.cpp
@@ -4,25 +4,75 @@
 
 using namespace std;
 
+void printVec(const vector<int> &v)
+{
+	auto i = v.begin();
+	while (i != v.end())
+		cout << *(i++) << " ";
+	cout << endl;
+}
+
+void pressAnyKeyToQuit()
+{
+	while(isspace(cin.peek()))
+		cin.get();
+	cin.get();
+}
+
 int main()
 {
+	int numInts;
+	cout << "Number of random integers to insert: ";
+	cin >> numInts;
+
 	RedBlackTree<int, double> tree;
-	/*for (int i = 0; i < 3; i++)
-		tree.insert(i, i * 2.0);*/
-	/*
-	RandomInt generator(0, 1000);
+	RandomInt keyGen(0, numInts * 2);
+	RandomInt indexGen(0, numInts - 1);
 
-	for (int i = 0; i < 100; i++)
+	cout << "Generating random keys..." << endl;
+	int i;
+	for (i = 0; i < numInts; i++)
 	{
-		int r = generator.get();
-		try {
-			tree.insert(r, r + 1.0);
-		} catch (exception e) { e.what(); }
-	}*/
+		keyGen.getUnique();
+		indexGen.getUnique();
+	}
 
-	for (int i = 1; i <= 10; i++)
-		tree.insert(i, 2.0*i);
-	char choice;
+	vector<int> numbers = keyGen.history();
+	vector<int> indices = indexGen.history();
+
+	//cout << "numbers: "; printVec(numbers);
+	//cout << "indices: "; printVec(indices);
+
+	cout << "Doing tree inserts..." << endl;
+	for (i = 0; i < numInts; i++)
+	{
+		int k = numbers[i];
+		double v = k + 1.0;
+		tree.insert(k, v);
+	}
+
+	cout << "Doing tree removals with error checking (may take awhile)..." << endl;
+
+	bool error = false;
+	for (i = 0; i < numInts; i++)
+	{
+		bool isBinary = tree.isBinary();
+		bool isConnected = tree.isConnected();
+		bool isRedBlack = tree.isRedBlackTree();
+		error = error || !(isBinary && isConnected && isRedBlack);
+		if (error)
+			break;
+
+		int index = indices[i];
+		int n = numbers[index];
+		tree.remove(n);
+	}
+
+	if (error)
+		cout << "There was a problem during the " << i << "th removal.";
+	else
+		cout << "No errors occurred.";
+	/*char choice;
 	do {
 		cout << "A)dd, R)emove, C)lear, Q)uit: "; cin >> choice;
 		switch (choice = tolower(choice))
@@ -55,7 +105,8 @@ int main()
 		cout << "Tree is " << (is_connected ? "" : "not ") << "connected." << endl;
 		cout << "Tree is " << (is_redblack ? "" : "not ") << "redblack." << endl;
 		cout << "Depth is " << tree.depth() << endl;
-	} while (choice != 'q');
-
+	} while (choice != 'q');*/
+	
+	pressAnyKeyToQuit();
 	return 0;
 }
