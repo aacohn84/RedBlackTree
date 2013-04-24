cbc653d8f25aa06f6e2d1838824693e0841486eb
Aaron Cohn <acohn@cogswell.edu>
2013-04-24 01:43:28 -0700

    Made get() static, created history list for uniquely generated integers, improved performance for selection of unique random integers.

--- RedBlackTree/RandomInt.cpp
+++ RedBlackTree/RandomInt.cpp
@@ -2,17 +2,21 @@
 #include <cstdlib>
 #include <ctime>
 #include <exception>
+#include <algorithm>
 
 using namespace std;
 
 RandomInt::RandomInt(void)
 {
 	srand((unsigned)time(NULL));
+	v = new vector<int>();
+	h = new vector<int>();
 }
 
 
 RandomInt::~RandomInt(void)
 {
+	delete v;
 }
 
 RandomInt::RandomInt(int min, int max)
@@ -25,6 +29,11 @@ RandomInt::RandomInt(int min, int max)
 	}
 	else
 		throw std::exception("Range error: min must be >= 0 and min < max.");
+
+	v = new vector<int>();
+	h = new vector<int>();
+	for (int i = 0; i < (max - min + 1); i++)
+		v->push_back(i);
 }
 
 int RandomInt::get()
@@ -32,6 +41,7 @@ int RandomInt::get()
 	return rand() % (max - min + 1) + min;
 }
 
+/* returns a random integer such that min <= n <= max */
 int RandomInt::get(int min, int max)
 {
 	return rand() % (max - min + 1) + min;
@@ -39,10 +49,35 @@ int RandomInt::get(int min, int max)
 
 int RandomInt::getUnique()
 {
-	return 0;
+	if (v->empty())
+		throw std::exception("Error: range exhausted");
+
+	// get a random number between 0 and the size of the range vector
+	int i = get(0, v->size() - 1);	// 0 <= i < v->size
+	auto it = v->begin() + i;
+
+	// n will be the value currently at that position
+	int n = *it;
+	h->push_back(n);
+
+	// erasing the element from the vector reduces the size of the vector and the pool of numbers to choose from
+	v->erase(it);
+	return n;
 }
 
-int RandomInt::getUnique(int min, int max)
+//int RandomInt::getUnique(int min, int max)
+//{
+//	int n;
+//	vector<int>::iterator it;
+//	do {
+//		n = rand() % (max - min + 1) + min;
+//		it = find(v->begin(), v->end(), n);
+//	} while (it != v->end());
+//	v->push_back(n);
+//	return n;
+//}
+
+const std::vector<int>& RandomInt::history()
 {
-	return 0;
+	return *h;
 }
--- RedBlackTree/RandomInt.h
+++ RedBlackTree/RandomInt.h
@@ -1,17 +1,23 @@
 #pragma once
+
+#include <vector>
+
 class RandomInt
 {
 private:
 	int min;
 	int max;
+	std::vector<int> *v;
+	std::vector<int> *h;
 
 public:
 	RandomInt(void);
 	RandomInt(int min, int max);
 	int get();
-	int get(int min, int max);
+	static int get(int min, int max);
 	int getUnique();
 	int getUnique(int min, int max);
+	const std::vector<int>& history();
 	~RandomInt(void);
 };
 
